09590979e1c5ba2162ad94bcb9a37613a6bdb35c
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-04-26 13:32:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 //your JS code here. If required.
 let input = prompt("Enter a string");
-for(let i = 0;i<input.length;i++) {
-	let flag = 0;
-	for(let j = 0;j<input.length;j++) {
-		if(j!=i) {
-			if(input.charAt(i)==input.charAt(j)) {
-				flag = 1;
-			}
-		}
-	}
-	if(flag==0) {
-		return input.charAt(i);
-	}
-}
-
+function func(input) {
+   for(let i = 0;i<input.length;i++) {
+      let flag = 0;
+      for(let j = 0;j<input.length;j++) {
+         if(j!=i) {
+            if(input.charAt(i)==input.charAt(j)) {
+               flag = 1;
+            }
+         }
+      }
+      if(flag==0) {
+         return input.charAt(i);
+      }
+   }
+   }
+   console.log(func(input));
 
